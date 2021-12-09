9ba8e6a7d0451f0cbb7d73fc7762892469dcd251
zhihong <173858902@qq.com>
2021-12-09 10:59:19 +0800

    import source

---
+++ zHFile/zHFile.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace zHFile.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
